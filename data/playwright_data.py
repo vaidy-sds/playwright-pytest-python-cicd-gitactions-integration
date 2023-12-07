65c781c8af89f69f4b214cee97d51a8e57e5e5bb
Vaidy Shanmugam <vaidy.sds@gmail.com>
2023-12-07 09:37:53 -0600

    created data structure

---
+++ data/playwright_data.py
@@ -0,0 +1,7 @@
+class PlaywrightData:
+
+    @staticmethod
+    def test_data():
+        return {
+            "url": "https://playwright.dev/"
+        }
--- models/playwright_home_page.py
+++ models/playwright_home_page.py
@@ -1,10 +1,12 @@
 from playwright.sync_api import Page, expect
 
+from data.playwright_data import PlaywrightData
+
 
 class PlaywrightHomePage:
     def __init__(self, page: Page):
         self.page = page
-        self.url = "https://playwright.dev/"
+        self.url = PlaywrightData().test_data()["url"]
         self.playwright_logo = self.page.get_by_role("link", name="Playwright logo Playwright")
 
     def goto(self):
