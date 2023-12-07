37a94c2bab849982e07989ba53f0ffe3d953e854
Vaidy Shanmugam <vaidy.sds@gmail.com>
2023-12-07 10:27:46 -0600

    turn off tracing by removing import for github actions to succeed

--- tests/test_website_checker.py
+++ tests/test_website_checker.py
@@ -1,6 +1,5 @@
 from playwright.sync_api import Page
 from models.playwright_home_page import PlaywrightHomePage
-from fixtures.trace_fixtures import trace_test
 
 
 def test_playwright_website(page: Page):
