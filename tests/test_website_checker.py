85523b6819a2a44c625c95633dad27b673ab6fcd
Vaidy Shanmugam <vaidy.sds@gmail.com>
2023-12-07 10:49:29 -0600

    reverting fixtures

--- tests/test_website_checker.py
+++ tests/test_website_checker.py
@@ -1,5 +1,6 @@
 from playwright.sync_api import Page
 from models.playwright_home_page import PlaywrightHomePage
+from fixtures.trace_fixtures import trace_test
 
 
 def test_playwright_website(page: Page):
